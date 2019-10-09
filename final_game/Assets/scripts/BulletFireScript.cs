26c9ba82ff4f7ff22823322a0c6f144f40d570e8
pokimoki99 <48154115+pokimoki99@users.noreply.github.com>
2019-10-09 13:49:49 +0100

    shoot, ammo and score

---
+++ final_game/Assets/scripts/BulletFireScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletFireScript : MonoBehaviour
+{
+
+    public Transform bulletprefab;
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")&&(GameManager.Instance.ammocount>0))
+        {
+            Instantiate(bulletprefab, transform.position, transform.rotation);
+            GameManager.Instance.fire();
+        }
+    }
+}
