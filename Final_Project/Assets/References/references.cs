bee0e094cf755410e88f9e992ecbf2d7b2759fa4
pokimoki99 <48154115+pokimoki99@users.noreply.github.com>
2019-11-01 12:03:29 +0000

    references scripts

---
+++ Final_Project/Assets/References/references.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class references : MonoBehaviour
+{
+
+
+
+    //https://blog.theknightsofunity.com/implementing-minimap-unity/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
