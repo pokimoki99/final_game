c7253939bd43ddadd96e8155a37fb5165a9277ec
pokimoki99 <48154115+pokimoki99@users.noreply.github.com>
2019-12-28 10:29:22 +0000

    gun script

---
+++ Final_Project/Assets/scripts 1/Guns.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guns : MonoBehaviour
+{
+
+    public bool pistol, SMG, Shotgun, Rifle, Sniper;    //checking gun
+
+    public float Pistoldps, SMGdps, Shotgundps, Rifledps, Sniperdps;    //checking damage
+
+    public float Pistolrange, SMGrange, Shotgunrange, Riflerange, Sniperrange;    //checking damage
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if tagged gun is active
+        //if tagged then adjust damage and ammo
+        
+    }
+}
