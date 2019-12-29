915e428b713f15410c42a0e2681ed1f739aa5873
pokimoki99 <48154115+pokimoki99@users.noreply.github.com>
2019-12-29 17:45:15 +0000

    attempt#3

--- Final_Project/Assets/scripts 1/Player.cs
+++ Final_Project/Assets/scripts 1/Player.cs
@@ -111,58 +111,7 @@ public class Player : MonoBehaviour
             }
             
         }
-        if (Input.GetKey(KeyCode.K))
-        {
-            score = 1;
-            GameManager.Instance.setscore(1);
-        }
-        if (Input.GetKey(KeyCode.L))
-        {
-            score = 0;
-            GameManager.Instance.setscore(0);
-        }
-        if (Input.GetKey(KeyCode.J))
-        {
-            score = 2;
-            GameManager.Instance.setscore(2);
-        }
-        if (Input.GetKey(KeyCode.H))
-        {
-            score = 3;
-            GameManager.Instance.setscore(3);
-        }
-        if (score == 0)
-        {
-            pistol.GetComponentInChildren<Renderer>().enabled = true;
-            //GameManager.Instance.Ammo()
-            spread.pistol_spread = true;
-            spread.shotgun_spread = false;
-            spread.Assault_rifle_spread = false;
-            spread.sniper_spread = false;
-
-        }
-        if (score == 1)
-        {
-            spread.pistol_spread = false;
-            spread.shotgun_spread = true;
-            spread.Assault_rifle_spread = false;
-            spread.sniper_spread = false;
-
-        }
-        if (score == 2)
-        {
-            spread.Assault_rifle_spread = true;
-            spread.shotgun_spread = false;
-            spread.pistol_spread = false;
-            spread.sniper_spread = false;
-        }
-        if (score == 3)
-        {
-            spread.sniper_spread = true;
-            spread.Assault_rifle_spread = false;
-            spread.shotgun_spread = false;
-            spread.pistol_spread = false;
-        }
+      
     }
     private void OnTriggerEnter(Collider other)
     {
