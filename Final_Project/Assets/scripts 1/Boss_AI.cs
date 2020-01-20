c293b17caeda50b29474c0fbaf36364ea554153d
pokimoki99 <48154115+pokimoki99@users.noreply.github.com>
2020-01-20 16:29:44 +0000

    boss doing damage

--- Final_Project/Assets/scripts 1/Boss_AI.cs
+++ Final_Project/Assets/scripts 1/Boss_AI.cs
@@ -9,6 +9,7 @@ public class Boss_AI : MonoBehaviour
     private UnityEngine.AI.NavMeshAgent nav;                                // Reference to the nav mesh agent.
     private Transform player;                               // Reference to the player's transform.
     private GameObject play;
+    Player_Health player_hp;
 
     public string text;
     public float timeBetweenAttacks = 3.0f;     // The time in seconds between each attack.
@@ -38,6 +39,9 @@ public class Boss_AI : MonoBehaviour
     public GameObject Punch;
     public GameObject Kick;
 
+    //collider tests
+    bool player_hit, Leg_Sweep_Hit, Swipe_Hit, Cast_Hit, Punch_Hit, Kick_Hit;
+
     void Awake()
     {
         // Setting up the references.
@@ -47,12 +51,21 @@ public class Boss_AI : MonoBehaviour
 
         player = GameObject.FindGameObjectWithTag("Player").transform;
 
+        player_hp = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Health>();
+
         anim = this.gameObject.GetComponent<Animator>();
 
         anim.SetBool("IsBattlecry",true);
         StartCoroutine(battlecry_timer());
 
+        Leg_Sweep.GetComponent<BoxCollider>().enabled = false;
+        Kick.GetComponent<BoxCollider>().enabled = false;
+        Swipe.GetComponent<BoxCollider>().enabled = false;
+        Cast.GetComponent<BoxCollider>().enabled = false;
+        Punch.GetComponent<BoxCollider>().enabled = false;
+
         //messagetext = GameObject.Find("MessageText").GetComponent<Text>();
+
 
     }
 
@@ -80,71 +93,128 @@ public class Boss_AI : MonoBehaviour
 
         }
 
-        if (timer >= timeBetweenAttacks && enemydist <= 5)
+        if (enemydist <= 4)
         {
 
             state = AIState.Attacking;
         }
-        Debug.Log(timer);
     }
 
 
     void Chasing()
     {
-        nav.speed = 0;
-        //nav.destination = player.transform.position;
-        //nav.speed = chaseSpeed;
-        //nav.isStopped = false;
+        //nav.speed = 0;
+        nav.destination = player.transform.position;
+        nav.speed = chaseSpeed;
+        nav.isStopped = false;
     }
 
     void Attacking()
     {
+        var rotation = Quaternion.LookRotation(player.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 0.5f);
         if (timer >= timeBetweenAttacks)
         { 
 
             nav.speed = 0;
             if (attack == 0)
             {
+                Leg_Sweep.GetComponent<BoxCollider>().enabled = false;
                 anim.SetBool("IsAttacking", true);
                 anim.SetFloat("attack", 0);
                 StartCoroutine(Attack_timer());
                 Debug.Log("Leg_Sweep");
-                //legsweep
+                //legswee
+                Leg_Sweep_Hit = true;
+
             }
             else if (attack == 1)
             {
+                Punch.GetComponent<BoxCollider>().enabled = false;
                 anim.SetBool("IsAttacking", true);
                 anim.SetFloat("attack", 0.25f);
                 StartCoroutine(Attack_timer());
                 Debug.Log("Swipe");
                 //clawattack
+                Swipe_Hit = true;
+
+
             }
             else if (attack == 2)
             {
+                Cast.GetComponent<BoxCollider>().enabled = true;
                 anim.SetBool("IsAttacking", true);
                 anim.SetFloat("attack", 0.5f);
                 StartCoroutine(Attack_timer());
                 Debug.Log("cast");
                 //cast
+                Cast_Hit = true;
+
+
             }
             else if (attack == 3)
             {
+                Swipe.GetComponent<BoxCollider>().enabled = true;
                 anim.SetBool("IsAttacking", true);
                 anim.SetFloat("attack", 0.75f);
                 StartCoroutine(Attack_timer());
                 Debug.Log("punch");
                 //swipe
+                Punch_Hit = true;
+
+
             }
             else if (attack == 4)
             {
+                Kick.GetComponent<BoxCollider>().enabled = true;
                 anim.SetBool("IsAttacking", true);
                 anim.SetFloat("attack", 1);
                 StartCoroutine(Attack_timer());
                 Debug.Log("kick");
+                Kick_Hit = true;
+
+
             }
 
 
 
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            player_hit = true;
+            if (player_hit)
+            {
+                if (Leg_Sweep_Hit)
+                {
+                    player_hp.health -= 30;
+                    Leg_Sweep_Hit = false;
+                }
+                else if (Swipe_Hit)
+                {
+                    player_hp.health -= 40;
+                    Swipe_Hit = false;
+                }
+                else if (Cast_Hit)
+                {
+                    player_hp.health -= 20;
+                    Cast_Hit = false;
+                }
+                else if (Punch_Hit)
+                {
+                    player_hp.health -= 30;
+                    Punch_Hit = false;
+                }
+                else if (Kick_Hit)
+                {
+                    player_hp.health -= 30;
+                    Kick_Hit = false;
+                }
+                player_hit = false;
+            }
         }
     }
 
@@ -152,13 +222,18 @@ public class Boss_AI : MonoBehaviour
     {
         attack = Random.Range(0, 4);
     }
-
     IEnumerator Attack_timer()
     {
         yield return new WaitForSeconds(2.8f);
         anim.SetBool("IsAttacking", false);
         timer = 0;
         attack_type();
+        Leg_Sweep.GetComponent<BoxCollider>().enabled = false;
+        Kick.GetComponent<BoxCollider>().enabled = false;
+        Swipe.GetComponent<BoxCollider>().enabled = false;
+        Cast.GetComponent<BoxCollider>().enabled = false;
+        Punch.GetComponent<BoxCollider>().enabled = false;
+
     }
    IEnumerator battlecry_timer()
     {
